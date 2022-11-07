6c34808f3a54022db28d5bc98611776142b96930
Mateusz Jabłoński <mail@mateuszjablonski.com>
2022-11-07 15:34:46 +0100

    task 5 solution

--- solution/music.js
+++ solution/music.js
@@ -25,12 +25,48 @@ Player.prototype.play = () => {
 
 function MusicPlayer() {
   this.playerWrapper = null;
+  this.isListFiltered = false;
+  this.songs = songs;
+  this.totalDuration = this.setTotalDuration();
+  this.totalDurationBox = document.createElement('div');
+
+  this.totalDurationBox.classList.add('total');
   Player.call(this);
   this.createPlayer();
+  this.updateTotalDuration();
 }
 
 MusicPlayer.prototype.createPlayer = function() {
   this.playerWrapper = document.createElement('div');
+
+  const sortButton = document.createElement('button');
+  sortButton.innerText = 'Sort by duration';
+  sortButton.classList.add('btn');
+
+  const filterEltonBtn = document.createElement('button');
+  filterEltonBtn.innerText = this.isListFiltered ? 'Reset filter' : 'Only Elton John Songs';
+  filterEltonBtn.classList.add('btn');
+
+  filterEltonBtn.addEventListener('click', () => {
+    if (this.isListFiltered) {
+      this.isListFiltered = false;
+      filterEltonBtn.innerText = 'Only Elton John Songs'
+      this.songs = songs;
+      this.updateList();
+    } else {
+      this.isListFiltered = true;
+      filterEltonBtn.innerText = 'Reset filter'
+      this.filterEltonSongs();
+    }
+  })
+
+  sortButton.addEventListener('click', () => {
+    this.sortByDuration();
+  });
+
+  this.playerWrapper.appendChild(filterEltonBtn);
+  this.playerWrapper.appendChild(sortButton);
+  this.playerWrapper.appendChild(this.totalDurationBox);
 
   const isDarkThemePrefered = isDarkThemePreffered();
 
@@ -41,7 +77,7 @@ MusicPlayer.prototype.createPlayer = function() {
   }
 
   this.playerWrapper.classList.add('player-wrapper');
-  this.playerWrapper.appendChild(this.prepareSongsList());
+  this.playerWrapper.appendChild(this.prepareSongsList(this.songs));
 
   document.body.insertAdjacentElement('afterbegin', this.playerWrapper);
 }
@@ -49,7 +85,7 @@ MusicPlayer.prototype.createPlayer = function() {
 MusicPlayer.prototype.prepareSongsList = function() {
   const list = document.createElement('ul');
 
-  songs
+  this.songs
     .map(({performer, duration, title}) => {
       const li = document.createElement('li');
 
@@ -58,12 +94,12 @@ MusicPlayer.prototype.prepareSongsList = function() {
       h2.classList.add('song-title');
 
       const performerElement = document.createElement('small');
-      performer.innerText = performer;
+      performerElement.innerText = performer;
       h2.appendChild(performerElement);
 
       const durationElement = document.createElement('div');
-      duration.classList.add('duration');
-      duration.innerText = formatDuration(duration);
+      durationElement.classList.add('duration');
+      durationElement.innerText = formatDuration(duration);
 
       li.appendChild(h2);
       li.appendChild(durationElement);
@@ -73,6 +109,32 @@ MusicPlayer.prototype.prepareSongsList = function() {
     .forEach(li => list.appendChild(li));
 
   return list;
+}
+
+MusicPlayer.prototype.setTotalDuration = function() {
+  return this.songs.reduce((acc, { duration }) => acc + duration, 0);
+}
+
+MusicPlayer.prototype.updateTotalDuration = function() {
+  this.totalDurationBox.innerText = formatDuration(this.totalDuration);
+}
+
+MusicPlayer.prototype.sortByDuration = function () {
+  this.songs.sort((a, b) => a.duration - b.duration);
+  this.updateList();
+}
+
+MusicPlayer.prototype.updateList = function() {
+  this.playerWrapper.replaceChild(this.prepareSongsList(), this.playerWrapper.querySelector('ul'));
+  
+  this.totalDuration = this.setTotalDuration();
+  this.updateTotalDuration();
+}
+
+MusicPlayer.prototype.filterEltonSongs = function() {
+  this.songs = this.songs.filter(song => song.performer === 'Elton John');
+
+  this.updateList();
 }
 
 Object.setPrototypeOf(MusicPlayer.prototype, Player.prototype);
