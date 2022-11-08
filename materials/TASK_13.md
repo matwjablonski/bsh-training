5ba8b89c2e9853a3a458229176c953e741403463
Mateusz Jabłoński <mail@mateuszjablonski.com>
2022-11-08 14:08:56 +0100

    task 13

---
+++ materials/TASK_13.md
@@ -0,0 +1,5 @@
+# Task 13
+
+Estimation: 20 min
+
+- replace importing data from songs.ts into fetching from api `http://localhost:8000/api/songs`
--- solution/task_12.ts
+++ solution/task_12.ts
@@ -3,7 +3,13 @@ enum ObjectType {
   PAPER = 'paper'
 }
 
-function prepareObject(data, type) {
+interface PreparedObject<T> {
+  type: ObjectType;
+  manufactured: number;
+  data: T;
+}
+
+function prepareObject<T = ObjectType>(data: T, type: ObjectType): PreparedObject<Omit<T, 'size'>> {
   return {
       type,
       manufactured: 2022,
@@ -11,8 +17,20 @@ function prepareObject(data, type) {
   }
 }
 
-const prepareColoredPen = (color) => prepareObject({ color }, ObjectType.PEN);
-const preparePaper = (color, size) => prepareObject({ color, size }, ObjectType.PAPER);
+interface Pen {
+  color: string;
+}
+
+type PaperSizes = 'A4' | 'A3';
+
+interface Paper extends Pen {
+  size:PaperSizes
+}
+
+const prepareColoredPen = (color: string): PreparedObject<Pen> => 
+  prepareObject<Pen>({ color }, ObjectType.PEN);
+const preparePaper = (color: string, size: PaperSizes): PreparedObject<Paper> =>
+  prepareObject<Paper>({ color, size }, ObjectType.PAPER);
 
 prepareColoredPen('red');
 preparePaper('white', 'A4');
